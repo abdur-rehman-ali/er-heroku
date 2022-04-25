0987d0e24bc7e025716fd3a8ae3147ccc0d7f093
abdur-rehman-ali <alijoyia911@gmail.com>
2022-04-25 11:52:15 +0500

    data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+30.times do |vote|
+    Voter.create(name:"Voter #{vote+1}",candidate_id:2)
+end
